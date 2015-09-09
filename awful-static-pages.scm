mario-goulart/awful-static-pages

commit 491ad6d981844f8fd1a823ca5f9b5cd275a9bdbb

Sort resources to get a deterministic order when writing files

awful-static-pages.scm
@@ -3,7 +3,7 @@
 (generate-static-pages! index-file request-pages)
 
 (import chicken scheme)
-(use files irregex posix srfi-1 srfi-13 srfi-69)
+(use data-structures files irregex posix srfi-1 srfi-13 srfi-69)
 (use awful)
 
 (define index-file (make-parameter "index.html"))
@@ -70,6 +70,8 @@
                ((procedure? path)
                 (write-static-page/procedure! path handler outdir))))))
    (or resources
-       (hash-table->alist (awful-resources-table)))))
+       (sort (hash-table->alist (awful-resources-table))
+             (lambda (a b)
+               (string< (caar a) (caar b)))))))
 
 ) ;; end module

commit c5c0749df24dddc82265fd6c385aad0c96c9c159

Handle procedures and regex objects when sorting
Fix issue introduced by 491ad6d98 (0.1.3)

awful-static-pages.scm
@@ -70,8 +70,20 @@
                ((procedure? path)
                 (write-static-page/procedure! path handler outdir))))))
    (or resources
-       (sort (hash-table->alist (awful-resources-table))
-             (lambda (a b)
-               (string< (caar a) (caar b)))))))
+       (let ((awful-resources (hash-table->alist (awful-resources-table))))
+         ;; Sort resources for a deterministic order: first
+         ;; procedures, then regex matchers, then sorted string
+         ;; matchers
+         (append (filter (lambda (res)
+                           (procedure? (caar res)))
+                         awful-resources)
+                 (filter (lambda (res)
+                           (irregex? (caar res)))
+                         awful-resources)
+                 (sort (filter (lambda (res)
+                                 (string? (caar res)))
+                               awful-resources)
+                       (lambda (a b)
+                         (string< (caar a) (caar b)))))))))
 
 ) ;; end module